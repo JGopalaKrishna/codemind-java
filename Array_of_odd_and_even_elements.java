6e1348b732f8cf1084c7d21de2d69146f8d3fee6
JGopalaKrishna <141515852+JGopalaKrishna@users.noreply.github.com>
2023-08-25 20:06:06 +0530

    import java.util.Scanner; public class test{     public static void main(String[] ahrs){         Scanner sc = new Scanner(System.in);         int a=sc.nextInt(); 	    int arr[]=new int[a]; 	    for(int i=0;i<a;i++){ 		arr[i]=sc.nextInt(); 	    } 		for(int j=0;j<a;j++){ 	            if(arr[j]%2!=0){ 	             System.out.print(arr[j]+" "); 	            }         }         for(int j=0;j<a;j++){ 	            if(arr[j]%2==0){ 	             System.out.print(arr[j]+" "); 	            }         }
        }
    }

---
+++ Array_of_odd_and_even_elements.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class test{
+    public static void main(String[] ahrs){
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+	    int arr[]=new int[a];
+	    for(int i=0;i<a;i++){
+		arr[i]=sc.nextInt();
+	    }
+		for(int j=0;j<a;j++){
+	            if(arr[j]%2!=0){
+	             System.out.print(arr[j]+" ");   
+	            }
+        }
+        for(int j=0;j<a;j++){
+	            if(arr[j]%2==0){
+	             System.out.print(arr[j]+" ");   
+	            }
+        }
+	    
+    }
+}
