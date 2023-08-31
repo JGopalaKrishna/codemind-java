692a861991529975ea86f1747e1dd15d85764758
JGopalaKrishna <141515852+JGopalaKrishna@users.noreply.github.com>
2023-08-31 20:35:08 +0530

    import java.util.Scanner; public class test{     public static void main(String[] ahrs){         Scanner sc = new Scanner(System.in);         int a=sc.nextInt();         int q,r,s=0; 	    while(a!=0 && a!=1 && a!=2 && a!=3 && a!=4 && a!=5 && a!=6 && a!=7 && a!=8 && a!=9){ 	        q=a; 	        while(q!=0){ 	            r=q%10; 	            s+=r*r; 	            q=q/10; 	        } 	        a=s; 	        s=0; 	    } 	    if(a==1 || a==7) System.out.printf("True"); 	    else System.out.printf("False");     } }

---
+++ happy_number.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class test{
+    public static void main(String[] ahrs){
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int q,r,s=0;
+	    while(a!=0 && a!=1 && a!=2 && a!=3 && a!=4 && a!=5 && a!=6 && a!=7 && a!=8 && a!=9){
+	        q=a; 
+	        while(q!=0){
+	            r=q%10;
+	            s+=r*r;
+	            q=q/10;
+	        }
+	        a=s;
+	        s=0;
+	    }
+	    if(a==1 || a==7) System.out.printf("True");
+	    else System.out.printf("False");
+    }
+}
