f66387cc97343ca80842245ac901b861dd75e899
JGopalaKrishna <141515852+JGopalaKrishna@users.noreply.github.com>
2023-08-31 19:20:53 +0530

    import java.util.Scanner; public class test{     public static void main(String[] ahrs){         Scanner sc = new Scanner(System.in);         int a=sc.nextInt(); 	    while(a!=(-1)){ 	        System.out.println(a*a); 	        a=sc.nextInt(); 	    }
        }
    }

---
+++ Infinite.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class test{
+    public static void main(String[] ahrs){
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+	    while(a!=(-1)){
+	        System.out.println(a*a);
+	        a=sc.nextInt();
+	    }
+	    
+    }
+}
