46baeb8d7cf79abe5053b1333d5fca5f54fbf77a
JGopalaKrishna <141515852+JGopalaKrishna@users.noreply.github.com>
2023-08-31 19:36:08 +0530

    import java.util.Scanner; public class test{     public static void main(String[] ahrs){         Scanner sc = new Scanner(System.in);         int a=sc.nextInt(); 	    int sum=0; 	    for(int i=1;i<a;i++){ 		    if(a%i==0) 		        sum+=1; 	    } 	    System.out.printf(sum==1?"Prime":"Not Prime");     } }

---
+++ Prime.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class test{
+    public static void main(String[] ahrs){
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+	    int sum=0;
+	    for(int i=1;i<a;i++){
+		    if(a%i==0)
+		        sum+=1;
+	    }
+	    System.out.printf(sum==1?"Prime":"Not Prime");
+    }
+}
