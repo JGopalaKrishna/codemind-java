88f76f4f7ba8512eb47a9cc81a48b60e78f6e8ad
JGopalaKrishna <141515852+JGopalaKrishna@users.noreply.github.com>
2023-09-06 21:02:34 +0530

    import java.util.Scanner; public class test{     public static void main(String[] ahrs){         Scanner sc = new Scanner(System.in);         int n=sc.nextInt(); 	    for(int i=0;i<n;i++){ 	        int a=sc.nextInt(); 	        int b=sc.nextInt(); 	        System.out.println(a+b); 	    }     } }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class test{
+    public static void main(String[] ahrs){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+	    for(int i=0;i<n;i++){
+	        int a=sc.nextInt();
+	        int b=sc.nextInt();
+	        System.out.println(a+b);
+	    }
+    }
+}
