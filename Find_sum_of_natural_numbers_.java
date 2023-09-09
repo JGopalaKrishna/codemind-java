981db81a6c9d75187cbe1a5fc7b63fc3dafd5666
JGopalaKrishna <141515852+JGopalaKrishna@users.noreply.github.com>
2023-09-09 16:07:15 +0530

    import java.util.Scanner; public class sumOfNatureNumber{     public static void main(String[] asw){         Scanner sc = new Scanner(System.in);         int n=sc.nextInt();         System.out.print((n*(n+1))/2);     } }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class sumOfNatureNumber{
+    public static void main(String[] asw){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.print((n*(n+1))/2);
+    }
+}
