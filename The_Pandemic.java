a4f40b808789cebca2ef7e92151760925cab8faa
JGopalaKrishna <141515852+JGopalaKrishna@users.noreply.github.com>
2023-10-17 14:50:17 +0530

    import java.util.*; public class as{     public static void main(String[] asd){         Scanner sc = new Scanner(System.in);         int n=sc.nextInt();         if(n==2) System.out.println(1);         else if(n>2) System.out.println(2);         else System.out.println(0);     } }

---
+++ The_Pandemic.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class as{
+    public static void main(String[] asd){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n==2) System.out.println(1);
+        else if(n>2) System.out.println(2);
+        else System.out.println(0);
+    }
+}
